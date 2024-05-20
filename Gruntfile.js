1166ad645c1a4f9ed84b667efc32c17d921ffa5a
PhilipeF <philipesferreiraa@gmail.com>
2024-05-20 16:47:57 -0300

    Feat: Configuração do Grunt - ok

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,19 @@ module.exports = function (grunt) {
                 files: {
                     'dev/styles/main.css': 'src/styles/main.less'
                 }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
             }
-        }
+        },
     })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('default', ['less:development'])
+    grunt.registerTask('build', ['less:production'])
 }
